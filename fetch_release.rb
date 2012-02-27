cacf66041aefdf36ac9a975c368bc7e4937bdacc
Alexandre Girard <git@alexgirard.com>
2012-02-27 19:56:45 +0100

    begin migration to mongomapper

--- fetch_release.rb
+++ fetch_release.rb
@@ -5,50 +5,58 @@ require 'date'
 require 'time'
 require 'fileutils'
 require 'redcarpet/compat'
+require 'mongo_mapper'
 
-def parse_table(table, existing_ids, new_content)
+MongoMapper.database = 'wikileaks_stratfor'
+
+class Document
+  include MongoMapper::Document
+
+  key :wikileaks_id, String
+  key :subject, String
+  key :href, String
+  key :date, Time
+
+  timestamps!
+end
+
+def parse_table(table, new_content)
   table.css("tr").each do |row|
     if cells = row.css("td")
       if cells[0]
         id = cells[0].text.chomp
-        unless existing_ids.include? id
+        unless Document.find_by_wikileaks_id(id)
           href = cells[0].css("a").first["href"]
           subject = cells[1].text.chomp
           date = DateTime.parse(cells[4].text + "-01")
-          new_content << {:id => id, :subject => subject, :href => href, :date => date}
-          existing_ids << id
+          new_content << Document.create(
+            :wikileaks_id => id,
+            :subject => subject,
+            :href => href,
+            :date => date
+          )
         end
       end
     end
   end
 end
 
-def parse_page(href, existing_ids, new_content)
+def parse_page(href, new_content)
   doc = Nokogiri::HTML(open("http://wikileaks.org#{href}"))
   if table = doc.css("table.cable")
-    parse_table(doc, existing_ids, new_content)
+    parse_table(doc, new_content)
 
     if next_link = doc.css(".pane.big a").select{|link| link.text == "Next"}.first
-      parse_page(next_link["href"], existing_ids, new_content)
+      parse_page(next_link["href"], new_content)
     end
   end
 end
 
-def parse_gifiles(existing_ids, new_content)
+def parse_gifiles(new_content)
   doc = Nokogiri::HTML(open("http://wikileaks.org/gifiles/releasedate/2012-02-27.html"))
   doc.css(".listoflist")[1].css("a").each do |date|
-    parse_page(date["href"], existing_ids, new_content)
+    parse_page(date["href"], new_content)
   end
-end
-
-def load_existing_ids
-  FileUtils.touch "existing_ids.md" unless File.exists? "existing_ids.md"
-  file = File.open("existing_ids.md", 'r')
-  return file.gets.split("-")
-end
-
-def save_existing_ids(existing_ids)
-  File.open("existing_ids.md", "w"){|f| f.write existing_ids.join("-")}
 end
 
 def save_content(content)
@@ -111,15 +119,14 @@ def create_html
 end
 
 def fetch_release(forced = false)
-  existing_ids = forced ? [] : load_existing_ids
   new_content = []
 
-  parse_gifiles(existing_ids, new_content)
+  parse_gifiles(new_content)
 
+  p new_content.size
   if new_content.size > 0
-    save_existing_ids(existing_ids)
-    save_content(new_content)
-    create_html
+    #save_content(new_content)
+    #create_html
   end
 end
 
